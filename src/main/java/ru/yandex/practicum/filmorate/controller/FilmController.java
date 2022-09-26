9cb071a7caece0fac88a8478c8da05e009c28577
egolubyh <egolubyh@yandex.ru>
2022-09-26 14:46:07 +0500

    feat: добавить логирование

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -11,13 +12,18 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@Slf4j
 public class FilmController {
     private final Map<Integer, Film> films = new HashMap<>();
     private int id;
 
     @PostMapping(value = "/films")
     public Film createFilm(@RequestBody Film film) throws ValidationException {
-        if (!valid(film)) throw new ValidationException();
+        log.info("Получен запрос к эндпоинту: /films, метод POST");
+        if (!valid(film)) {
+            log.debug("Ошибка");
+            throw new ValidationException();
+        }
         film.setId(++id);
         films.put(film.getId(), film);
         return film;
@@ -25,13 +31,18 @@ public class FilmController {
 
     @PutMapping(value = "/films")
     public Film updateFilm(@RequestBody Film film) throws ValidationException {
-        if (!films.containsKey(film.getId()) || !valid(film)) throw new ValidationException();
+        log.info("Получен запрос к эндпоинту: /films, метод PUT");
+        if (!films.containsKey(film.getId()) || !valid(film)) {
+            log.debug("Ошибка");
+            throw new ValidationException();
+        }
         films.put(film.getId(), film);
         return film;
     }
 
     @GetMapping("/films")
     public List<Film> findAllFilms() {
+        log.info("Получен запрос к эндпоинту: /films, метод GET");
         return new ArrayList<>(films.values());
     }
 
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@Slf4j
 public class UserController {
     private final Map<Integer, User> users = new HashMap<>();
 
@@ -18,7 +20,11 @@ public class UserController {
 
     @PostMapping(value = "/users")
     public User createUser(@RequestBody User user) throws ValidationException {
-        if (!valid(user)) throw new ValidationException();
+        log.info("Получен запрос к эндпоинту: /users, метод POST");
+        if (!valid(user)) {
+            log.debug("Ошибка");
+            throw new ValidationException();
+        }
         user.setId(++id);
         users.put(user.getId(), user);
         return user;
@@ -26,13 +32,18 @@ public class UserController {
 
     @PutMapping(value = "/users")
     public User updateUser(@RequestBody User user) throws ValidationException {
-        if (!users.containsKey(user.getId()) || !valid(user)) throw new ValidationException();
+        log.info("Получен запрос к эндпоинту: /users, метод PUT");
+        if (!users.containsKey(user.getId()) || !valid(user)) {
+            log.debug("Ошибка");
+            throw new ValidationException();
+        }
         users.put(user.getId(), user);
         return user;
     }
 
     @GetMapping("/users")
     public List<User> findAllUsers() {
+        log.info("Получен запрос к эндпоинту: /users, метод GET");
         return new ArrayList<>(users.values());
     }
 
