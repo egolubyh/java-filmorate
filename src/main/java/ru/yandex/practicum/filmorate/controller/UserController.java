47d6aaf97e9781d428c1ad90653d5bdc44f8b460
egolubyh <egolubyh@yandex.ru>
2022-10-03 21:42:22 +0500

    fix: исправить на работу с InMemoryUserStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,22 +1,24 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.ValidationService;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-@RestController
 @Slf4j
+@RestController
 public class UserController {
-    private final Map<Integer, User> users = new HashMap<>();
+    private final InMemoryUserStorage userStorage;
 
-    private int id;
+    @Autowired
+    public UserController(InMemoryUserStorage userStorage) {
+        this.userStorage = userStorage;
+    }
 
     @PostMapping(value = "/users")
     public User createUser(@RequestBody User user) throws ValidationException {
@@ -25,25 +27,24 @@ public class UserController {
             log.debug("Ошибка");
             throw new ValidationException();
         }
-        user.setId(++id);
-        users.put(user.getId(), user);
-        return user;
+
+        return userStorage.createUser(user);
     }
 
     @PutMapping(value = "/users")
     public User updateUser(@RequestBody User user) throws ValidationException {
         log.info("Получен запрос к эндпоинту: /users, метод PUT");
-        if (!users.containsKey(user.getId()) || !ValidationService.valid(user)) {
+        if (!userStorage.userIsExist(user.getId()) || !ValidationService.valid(user)) {
             log.debug("Ошибка");
             throw new ValidationException();
         }
-        users.put(user.getId(), user);
-        return user;
+
+        return userStorage.updateUser(user);
     }
 
     @GetMapping("/users")
     public List<User> findAllUsers() {
         log.info("Получен запрос к эндпоинту: /users, метод GET");
-        return new ArrayList<>(users.values());
+        return userStorage.findAll();
     }
 }
