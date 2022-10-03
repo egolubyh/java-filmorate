d7b149ff9232348426086b7066e3661ab88e542a
egolubyh <egolubyh@yandex.ru>
2022-10-03 17:03:20 +0500

    fix: исправить на работу с InMemoryUserStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,21 +1,23 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.ValidationService;
+import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-@RestController
 @Slf4j
+@RestController
 public class FilmController {
-    private final Map<Integer, Film> films = new HashMap<>();
-    private int id;
+    private final InMemoryFilmStorage filmStorage;
+    @Autowired
+    public FilmController(InMemoryFilmStorage filmStorage) {
+        this.filmStorage = filmStorage;
+    }
 
     @PostMapping(value = "/films")
     public Film createFilm(@RequestBody Film film) throws ValidationException {
@@ -24,25 +26,25 @@ public class FilmController {
             log.debug("Ошибка");
             throw new ValidationException();
         }
-        film.setId(++id);
-        films.put(film.getId(), film);
-        return film;
+
+        return filmStorage.createFilm(film);
     }
 
     @PutMapping(value = "/films")
     public Film updateFilm(@RequestBody Film film) throws ValidationException {
         log.info("Получен запрос к эндпоинту: /films, метод PUT");
-        if (!films.containsKey(film.getId()) || !ValidationService.valid(film)) {
+        if (!filmStorage.filmIsExist(film.getId()) || !ValidationService.valid(film)) {
             log.debug("Ошибка");
             throw new ValidationException();
         }
-        films.put(film.getId(), film);
-        return film;
+
+        return filmStorage.updateFilm(film);
     }
 
     @GetMapping("/films")
     public List<Film> findAllFilms() {
         log.info("Получен запрос к эндпоинту: /films, метод GET");
-        return new ArrayList<>(films.values());
+
+        return filmStorage.findAll();
     }
 }
