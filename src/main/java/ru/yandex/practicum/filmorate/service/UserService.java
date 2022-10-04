411cfb1152d013dffa7a753bfefadf8ba2ad1eb5
egolubyh <egolubyh@yandex.ru>
2022-10-04 15:27:00 +0500

    add UserService.java

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -0,0 +1,46 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+    private final InMemoryUserStorage userStorage;
+
+    @Autowired
+    public UserService(InMemoryUserStorage userStorage) {
+        this.userStorage = userStorage;
+    }
+
+    public void addFriend(int userId, int friendId) {
+        User user = userStorage.findUserById(userId);
+        User friend = userStorage.findUserById(friendId);
+
+        user.getFriends().add(friendId);
+        friend.getFriends().add(userId);
+    }
+
+    public void deleteFriend(int userId, int friendId) {
+        User user = userStorage.findUserById(userId);
+        User friend = userStorage.findUserById(friendId);
+
+        user.getFriends().remove(friendId);
+        friend.getFriends().remove(userId);
+    }
+
+    public List<User> findAllMutualFriends(int userId, int friendId) {
+        Set<Integer> user = userStorage.findUserById(userId).getFriends();
+        Set<Integer> friends = userStorage.findUserById(friendId).getFriends();
+
+        friends.retainAll(user);
+        return friends.stream()
+                .map(userStorage::findUserById)
+                .collect(Collectors.toList());
+    }
+}
