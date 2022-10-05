8bb323b54fa4b338f74a17659f1e3ad8f598c346
egolubyh <egolubyh@yandex.ru>
2022-10-05 15:21:56 +0500

    feat: добавить эндпоинты ТЗ-10

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -3,42 +3,94 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.service.ValidationService;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 
 import java.util.List;
 
 @Slf4j
 @RestController
 public class FilmController {
+
     private final InMemoryFilmStorage filmStorage;
+    private final FilmService filmService;
+    private final InMemoryUserStorage userStorage;
+    private final ValidationService validationService;
     @Autowired
-    public FilmController(InMemoryFilmStorage filmStorage) {
+    public FilmController(InMemoryFilmStorage filmStorage,
+                          FilmService filmService,
+                          InMemoryUserStorage userStorage,
+                          ValidationService validationService) {
         this.filmStorage = filmStorage;
+        this.filmService = filmService;
+        this.userStorage = userStorage;
+        this.validationService = validationService;
     }
 
-    @PostMapping(value = "/films")
+    @PostMapping("/films")
     public Film createFilm(@RequestBody Film film) throws ValidationException {
         log.info("Получен запрос к эндпоинту: /films, метод POST");
-        if (!ValidationService.valid(film)) {
-            log.debug("Ошибка");
+        if (!validationService.isValid(film)) {
+            log.error("Ошибка валидации, недопустимые поля Film");
             throw new ValidationException();
         }
 
         return filmStorage.createFilm(film);
     }
 
-    @PutMapping(value = "/films")
-    public Film updateFilm(@RequestBody Film film) throws ValidationException {
+    @PutMapping("/films")
+    public Film updateFilm(@RequestBody Film film) throws Exception {
         log.info("Получен запрос к эндпоинту: /films, метод PUT");
-        if (!filmStorage.filmIsExist(film.getId()) || !ValidationService.valid(film)) {
-            log.debug("Ошибка");
+        if (filmStorage.idNotExist(film.getId())) {
+            log.error("Ошибка, фильма с таким id = " + film.getId() + " не существует.");
+            throw new NotFoundException(film.getId());
+        }
+
+        if (!validationService.isValid(film)) {
+            log.error("Ошибка валидации, недопустимые поля Film");
             throw new ValidationException();
         }
 
         return filmStorage.updateFilm(film);
+    }
+
+    @PutMapping("/films/{id}/like/{userId}")
+    public void addLike(@PathVariable int id,
+                        @PathVariable int userId)  throws NotFoundException {
+        log.info("Получен запрос к эндпоинту: /films/{id}/like/{userId}, метод PUT");
+        if (filmStorage.idNotExist(id)) {
+            log.error("Ошибка, фильма с таким id = " + id + " не существует.");
+            throw new NotFoundException(id);
+        }
+
+        if (userStorage.idNotExist(userId)) {
+            log.error("Ошибка, пользователя с таким id = " + userId + " не существует.");
+            throw new NotFoundException(userId);
+        }
+
+        filmService.addLike(id,userId);
+    }
+
+    @DeleteMapping("/films/{id}/like/{userId}")
+    public void deleteLike(@PathVariable int id,
+                           @PathVariable int userId)  throws NotFoundException {
+        log.info("Получен запрос к эндпоинту: /films/{id}/like/{userId}, метод DELETE");
+        if (filmStorage.idNotExist(id)) {
+            log.error("Ошибка, фильма с таким id = " + id + " не существует.");
+            throw new NotFoundException(id);
+        }
+
+        if (userStorage.idNotExist(userId)) {
+            log.error("Ошибка, пользователя с таким id = " + userId + " не существует.");
+            throw new NotFoundException(userId);
+        }
+
+        filmService.deleteLike(id,userId);
     }
 
     @GetMapping("/films")
@@ -46,5 +98,24 @@ public class FilmController {
         log.info("Получен запрос к эндпоинту: /films, метод GET");
 
         return filmStorage.findAll();
+    }
+
+    @GetMapping("/films/{id}")
+    public Film findFilmById(@PathVariable int id) throws NotFoundException {
+        log.info("Получен запрос к эндпоинту: /films/{id}, метод GET");
+        if (filmStorage.idNotExist(id)) {
+            log.error("Ошибка, фильма с таким id = " + id + " не существует.");
+            throw new NotFoundException(id);
+        }
+
+        return filmStorage.findFilmById(id);
+    }
+
+    @GetMapping("/films/popular")
+    public List<Film> findMostPopularFilms(
+            @RequestParam(defaultValue = "10", required = false) int count) {
+        log.info("Получен запрос к эндпоинту: /films/popular, метод GET");
+
+        return filmService.findMostPopularFilms(count);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,8 +3,10 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.service.ValidationService;
 import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 
@@ -14,37 +16,119 @@ import java.util.List;
 @RestController
 public class UserController {
     private final InMemoryUserStorage userStorage;
+    private final UserService userService;
+    private final ValidationService validationService;
 
     @Autowired
-    public UserController(InMemoryUserStorage userStorage) {
+    public UserController(InMemoryUserStorage userStorage,
+                          UserService userService,
+                          ValidationService validationService) {
         this.userStorage = userStorage;
+        this.userService = userService;
+        this.validationService = validationService;
     }
 
-    @PostMapping(value = "/users")
+    @PostMapping("/users")
     public User createUser(@RequestBody User user) throws ValidationException {
         log.info("Получен запрос к эндпоинту: /users, метод POST");
-        if (!ValidationService.valid(user)) {
-            log.debug("Ошибка");
+        if (!validationService.isValid(user)) {
+            log.error("Ошибка валидации, недопустимые поля User");
             throw new ValidationException();
         }
 
         return userStorage.createUser(user);
     }
 
-    @PutMapping(value = "/users")
-    public User updateUser(@RequestBody User user) throws ValidationException {
+    @PutMapping("/users")
+    public User updateUser(@RequestBody User user) throws Exception {
         log.info("Получен запрос к эндпоинту: /users, метод PUT");
-        if (!userStorage.userIsExist(user.getId()) || !ValidationService.valid(user)) {
-            log.debug("Ошибка");
+        if (userStorage.idNotExist(user.getId())) {
+            log.error("Ошибка, пользователя с таким id = " + user.getId() + " не существует.");
+            throw new NotFoundException(user.getId());
+        }
+
+        if (!validationService.isValid(user)) {
+            log.error("Ошибка валидации, недопустимые поля User");
             throw new ValidationException();
         }
 
         return userStorage.updateUser(user);
     }
 
+    @PutMapping("/users/{id}/friends/{friendId}")
+    public void addFriends(@PathVariable int id,
+                           @PathVariable int friendId) throws NotFoundException {
+        log.info("Получен запрос к эндпоинту: /users/{id}/friends/{friendId}, метод PUT");
+        if (userStorage.idNotExist(id)) {
+            log.error("Ошибка, пользователя с таким id = " + id + " не существует.");
+            throw new NotFoundException(id);
+        }
+        if (userStorage.idNotExist(friendId)) {
+            log.error("Ошибка, пользователя с таким id = " + friendId + " не существует.");
+            throw new NotFoundException(friendId);
+        }
+
+        userService.addFriend(id,friendId);
+    }
+
+    @DeleteMapping("/users/{id}/friends/{friendId}")
+    public void deleteFriends(@PathVariable int id,
+                              @PathVariable int friendId) throws NotFoundException {
+        log.info("Получен запрос к эндпоинту: /users/{id}/friends/{friendId}, метод DELETE");
+        if (userStorage.idNotExist(id)) {
+            log.error("Ошибка, пользователя с таким id = " + id + " не существует.");
+            throw new NotFoundException(id);
+        }
+        if (userStorage.idNotExist(friendId)) {
+            log.error("Ошибка, пользователя с таким id = " + friendId + " не существует.");
+            throw new NotFoundException(friendId);
+        }
+
+        userService.deleteFriend(id,friendId);
+    }
+
     @GetMapping("/users")
     public List<User> findAllUsers() {
         log.info("Получен запрос к эндпоинту: /users, метод GET");
+
         return userStorage.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User findUserById(@PathVariable int id) throws Exception {
+        log.info("Получен запрос к эндпоинту: /users/{id}, метод GET");
+        if (userStorage.idNotExist(id)) {
+            log.error("Ошибка, пользователя с таким id = " + id + " не существует.");
+            throw new NotFoundException(id);
+        }
+        return userStorage.findUserById(id);
+    }
+
+    @GetMapping("/users/{id}/friends")
+    public List<User> findAllFriendsUserById(@PathVariable int id) throws NotFoundException {
+        log.info("Получен запрос к эндпоинту: /users/{id}/friends, метод GET");
+        if (userStorage.idNotExist(id)) {
+            log.error("Ошибка, пользователя с таким id = " + id + "не существует.");
+            throw new NotFoundException(id);
+        }
+
+        return userStorage.findAllFriendsUserById(id);
+    }
+
+    @GetMapping("/users/{id}/friends/common/{otherId}")
+    public List<User> findAllMutualFriends(@PathVariable int id,
+                           @PathVariable int otherId) throws NotFoundException {
+        log.info("Получен запрос к эндпоинту: /users/{id}/friends/common/{otherId}, метод GET");
+        if (userStorage.idNotExist(id)) {
+            log.error("Ошибка, пользователя с таким id = " + id + "не существует.");
+            throw new NotFoundException(id);
+        }
+
+        if (userStorage.idNotExist(otherId)) {
+            log.error("Ошибка, пользователя с таким id = " + otherId + "не существует.");
+            throw new NotFoundException(otherId);
+        }
+
+        return userService.findAllMutualFriends(id,otherId);
     }
 }
