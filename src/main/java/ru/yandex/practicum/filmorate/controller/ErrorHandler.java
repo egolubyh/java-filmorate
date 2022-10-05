82b582b5a4966fea9c71ac5219304c1870fb0459
egolubyh <egolubyh@yandex.ru>
2022-10-05 15:16:34 +0500

    add ErrorHandler.java

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+
+@RestControllerAdvice("ru.yandex.practicum.filmorate.controller")
+public class ErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleException(final Exception e) {
+        return "Произошла непредвиденная ошибка.";
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFoundException(final NotFoundException e) {
+        return "Произошла ошибка, переданный id = " + e.getId() + "не существует.";
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleValidationException(final ValidationException e) {
+        return "Произошла ошибка валидации" ;
+    }
+}
