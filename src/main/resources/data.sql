08d64c5596a2add239300bc1c8393d6c9fba243c
egolubyh <egolubyh@yandex.ru>
2022-11-09 12:24:05 +0500

    fix: добавить очистку таблиц

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,81 +1,93 @@
-MERGE INTO users VALUES
-    ( 1, 'Алексей', 'alex', 'alex@mail.com', '1991-01-01'),
-    ( 2, 'Егор', 'egor', 'egor@mail.com', '1992-02-02'),
-    ( 3, 'Борис', 'boris', 'boris@mail.com', '1993-03-03'),
-    ( 4, 'Владимир', 'vladimir', 'vladimir@mail.com', '1994-04-04'),
-    ( 5, 'Валентин', 'valentin', 'valentin@mail.com', '1995-05-05'),
-    ( 6, 'Геннадий', 'gena', 'gena@mail.com', '1996-06-06'),
-    ( 7, 'Григорий', 'grig', 'grig@mail.com', '1997-07-07'),
-    ( 8, 'Дмитрий', 'dima', 'dima@mail.com', '1997-07-07'),
-    ( 9, 'Максим', 'max', 'max@mail.com', '1998-08-08'),
-    ( 10, 'Михаил', 'misha', 'misha@mail.com', '1998-09-09');
+DELETE FROM LIKES;
+DELETE FROM FILM_GENRE;
+DELETE FROM FRIENDS;
+DELETE FROM USERS;
+DELETE FROM FILM;
 
-MERGE INTO friends VALUES
-    ( 1, 1, 2, TRUE),
-    ( 2, 2, 1, TRUE),
-    ( 3, 1, 3, TRUE),
-    ( 4, 3, 1, TRUE),
-    ( 5, 1, 4, TRUE),
-    ( 6, 4, 1, TRUE),
-    ( 7, 2, 3, TRUE),
-    ( 8, 3, 2, TRUE),
-    ( 9, 8, 9, FALSE),
-    ( 10, 8, 10, FALSE);
+ALTER TABLE LIKES ALTER COLUMN ID RESTART WITH 1;
+ALTER TABLE FILM_GENRE ALTER COLUMN ID RESTART WITH 1;
+ALTER TABLE FRIENDS ALTER COLUMN ID RESTART WITH 1;
+ALTER TABLE USERS ALTER COLUMN ID RESTART WITH 1;
+ALTER TABLE FILM ALTER COLUMN ID RESTART WITH 1;
 
-MERGE INTO rating VALUES
-    ( 1, 'G'),
-    ( 2, 'PG'),
-    ( 3, 'PG-13'),
-    ( 4, 'R'),
-    ( 5, 'NC-17');
+MERGE INTO RATING
+    VALUES ( 1, 'G'),
+           ( 2, 'PG'),
+           ( 3, 'PG-13'),
+           ( 4, 'R'),
+           ( 5, 'NC-17');
 
-MERGE INTO genre VALUES
-    ( 1, 'Комедия' ),
-    ( 2, 'Драма' ),
-    ( 3, 'Мультфильм' ),
-    ( 4, 'Триллер' ),
-    ( 5, 'Документальный' ),
-    ( 6, 'Боевик' );
+MERGE INTO GENRE
+    VALUES ( 1, 'Комедия' ),
+           ( 2, 'Драма' ),
+           ( 3, 'Мультфильм' ),
+           ( 4, 'Триллер' ),
+           ( 5, 'Документальный' ),
+           ( 6, 'Боевик' );
 
-MERGE INTO film VALUES
-    ( 1, 'Король Лев',  'desc', '1994-10-10', 88, 1),
-    ( 2, 'Аладдин',  'desc', '1992-10-10', 90, 1),
-    ( 3, 'Улика',  'desc', '1985-04-10', 94, 2),
-    ( 4, 'Контакт',  'desc', '1997-04-10', 150, 2),
-    ( 5, 'Начало',  'desc', '2010-11-10', 148, 3),
-    ( 6, 'Престиж',  'desc', '2006-10-10', 125, 3),
-    ( 7, 'Титаник',  'desc', '1997-10-10', 194, 3),
-    ( 8, 'Леон',  'desc', '1994-10-10', 133, 4),
-    ( 9, 'Бойцовский клуб',  'desc', '1999-10-10', 139, 4),
-    ( 10, 'Остров проклятых',  'desc', '2009-10-10', 138, 4),
-    ( 11, 'Святая кровь',  'desc', '1989-10-10', 125, 5),
-    ( 12, 'Трудная мишень',  'desc', '1993-10-10', 122, 5);
+INSERT INTO USERS (NAME, LOGIN, EMAIL, BIRTHDAY)
+    VALUES ('Алексей', 'alex', 'alex@mail.com', '1991-01-01'),
+           ('Егор', 'egor', 'egor@mail.com', '1992-02-02'),
+           ('Борис', 'boris', 'boris@mail.com', '1993-03-03'),
+           ('Владимир', 'vladimir', 'vladimir@mail.com', '1994-04-04'),
+           ('Валентин', 'valentin', 'valentin@mail.com', '1995-05-05'),
+           ('Геннадий', 'gena', 'gena@mail.com', '1996-06-06'),
+           ('Григорий', 'grig', 'grig@mail.com', '1997-07-07'),
+           ('Дмитрий', 'dima', 'dima@mail.com', '1997-07-07'),
+           ('Максим', 'max', 'max@mail.com', '1998-08-08'),
+           ('Михаил', 'misha', 'misha@mail.com', '1998-09-09');
 
-MERGE INTO film_genre VALUES
-    (1, 1, 3),
-    (2, 2, 3),
-    (3, 3, 1),
-    (4, 4, 2),
-    (5, 5, 4),
-    (6, 5, 5),
-    (7, 6, 5),
-    (8, 7, 6),
-    (9, 7, 1),
-    (10, 7, 2),
-    (11, 12, 6);
+INSERT INTO FRIENDS (FRIEND_ONE, FRIEND_TWO, CONFIRMED)
+    VALUES (1, 2, TRUE),
+           (2, 1, TRUE),
+           (1, 3, TRUE),
+           (3, 1, TRUE),
+           (1, 4, TRUE),
+           (4, 1, TRUE),
+           (2, 3, TRUE),
+           (3, 2, TRUE),
+           (8, 9, FALSE),
+           (8, 10, FALSE);
 
-MERGE INTO likes VALUES
-    ( 1, 1, 1 ),
-    ( 2, 1, 2 ),
-    ( 3, 1, 3 ),
-    ( 4, 2, 4 ),
-    ( 5, 2, 5 ),
-    ( 6, 3, 6 ),
-    ( 7, 4, 7 ),
-    ( 8, 4, 8 ),
-    ( 9, 5, 9 ),
-    ( 10, 6, 10 ),
-    ( 11, 6, 11 ),
-    ( 12, 7, 12 ),
-    ( 13, 8, 12 ),
-    ( 14, 9, 12 );
+INSERT INTO FILM (TITLE, DESCRIPTION, RELEASE_DATE, DURATION, RATING)
+    VALUES ('Король Лев',  'desc', '1994-10-10', 88, 1),
+           ('Аладдин',  'desc', '1992-10-10', 90, 1),
+           ('Улика',  'desc', '1985-04-10', 94, 2),
+           ('Контакт',  'desc', '1997-04-10', 150, 2),
+           ('Начало',  'desc', '2010-11-10', 148, 3),
+           ('Престиж',  'desc', '2006-10-10', 125, 3),
+           ('Титаник',  'desc', '1997-10-10', 194, 3),
+           ('Леон',  'desc', '1994-10-10', 133, 4),
+           ('Бойцовский клуб',  'desc', '1999-10-10', 139, 4),
+           ('Остров проклятых',  'desc', '2009-10-10', 138, 4),
+           ('Святая кровь',  'desc', '1989-10-10', 125, 5),
+           ('Трудная мишень',  'desc', '1993-10-10', 122, 5);
+
+INSERT INTO FILM_GENRE (FILM, GENRE)
+    VALUES (1, 3),
+           (2, 3),
+           (3, 1),
+           (4, 2),
+           (5, 4),
+           (5, 5),
+           (6, 5),
+           (7, 6),
+           (7, 1),
+           (7, 2),
+           (12, 6);
+
+INSERT INTO LIKES (USER_ID, FILM_ID)
+    VALUES (1, 1 ),
+           (1, 2 ),
+           (1, 3 ),
+           (2, 4 ),
+           (2, 5 ),
+           (3, 6 ),
+           (4, 7 ),
+           (4, 8 ),
+           (5, 9 ),
+           (6, 10 ),
+           (6, 11 ),
+           (7, 12 ),
+           (8, 12 ),
+           (9, 12 );
