cc5e288c7a9782286fd4ff93311734ca2a0144d0
egolubyh <egolubyh@yandex.ru>
2022-11-12 19:17:25 +0500

    add FilmDbStorage.java

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
@@ -0,0 +1,136 @@
+package ru.yandex.practicum.filmorate.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Component
+public class FilmDbStorage implements FilmStorage {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final GenreDbStorage genreDbStorage;
+    private final MpaDbStorage mpaDbStorage;
+
+    @Autowired
+    public FilmDbStorage(JdbcTemplate jdbcTemplate, GenreDbStorage genreDbStorage, MpaDbStorage mpaDbStorage) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.genreDbStorage = genreDbStorage;
+        this.mpaDbStorage = mpaDbStorage;
+    }
+
+
+    @Override
+    public Film createFilm(Film film) {
+        String sqlQuery = "INSERT INTO FILM (NAME, DESCRIPTION, RELEASEDATE, DURATION, RATE, MPA) " +
+                "VALUES ( ?, ?, ?, ?, ?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement stmt = connection.prepareStatement(sqlQuery, new String[]{"id"});
+            stmt.setString(1, film.getName());
+            stmt.setString(2, film.getDescription());
+            stmt.setDate(3, Date.valueOf(film.getReleaseDate()));
+            stmt.setInt(4, film.getDuration());
+            stmt.setInt(5, film.getRate());
+            stmt.setLong(6, film.getMpa().getId());
+            return stmt;
+        }, keyHolder);
+
+        film.setId(Objects.requireNonNull(keyHolder.getKey()).longValue());
+
+        if (film.getGenres() != null) {
+            genreDbStorage.createFilmGenre(film.getId(), film.getGenres());
+        }
+
+        return film;
+    }
+
+    @Override
+    public Film readFilm(long id) {
+        String sqlQuery = "SELECT ID, NAME, DESCRIPTION, RELEASEDATE, DURATION, RATE, MPA " +
+                " FROM FILM WHERE ID = ?";
+
+        return jdbcTemplate.queryForObject(sqlQuery, this::mapRowToFilm, id);
+    }
+
+    @Override
+    public List<Film> readAllFilms() {
+        String sqlQuery = "SELECT ID, NAME, DESCRIPTION, RELEASEDATE, DURATION, RATE, MPA " +
+                " FROM FILM";
+
+        return jdbcTemplate.query(sqlQuery, this::mapRowToFilm);
+    }
+
+    @Override
+    public Film updateFilm(Film film) {
+        String sqlQuery = "UPDATE FILM SET " +
+                "NAME = ?, DESCRIPTION = ?, RELEASEDATE = ?, DURATION = ?, RATE = ?, MPA = ?" +
+                "WHERE ID = ?";
+        jdbcTemplate.update(sqlQuery,
+                film.getName(),
+                film.getDescription(),
+                film.getReleaseDate(),
+                film.getDuration(),
+                film.getRate(),
+                film.getMpa().getId(),
+                film.getId());
+
+        if (film.getGenres() != null) {
+            List<Genre> list = film.getGenres()
+                    .stream()
+                    .distinct()
+                    .collect(Collectors.toList());
+            film.setGenres(list);
+            genreDbStorage.deleteFilmGenre(film.getId());
+            genreDbStorage.createFilmGenre(film.getId(), list);
+        }
+
+        return film;
+    }
+
+    @Override
+    public Film deleteFilm(Film film) {
+        String sqlQuery = "DELETE FROM FILM WHERE ID = ?";
+
+        jdbcTemplate.update(sqlQuery, film.getId());
+        return film;
+    }
+
+    @Override
+    public boolean idNotExist(long id) {
+        String sqlQuery = "SELECT EXISTS(SELECT * FROM FILM WHERE ID = ?)";
+
+        return Boolean.FALSE.equals(jdbcTemplate.queryForObject(sqlQuery,
+                new Object[]{id}, Boolean.class));
+    }
+
+    private Film mapRowToFilm(ResultSet rs, int rowNum) throws SQLException {
+        long filmId = rs.getLong("ID");
+        long mpaId = rs.getLong("MPA");
+
+        Film film = new Film();
+        film.setId(filmId);
+        film.setName(rs.getString("NAME"));
+        film.setDescription(rs.getString("DESCRIPTION"));
+        film.setReleaseDate(rs.getDate("RELEASEDATE").toLocalDate());
+        film.setDuration(rs.getInt("DURATION"));
+        film.setRate(rs.getInt("RATE"));
+        film.setMpa(mpaDbStorage.readMpa(mpaId));
+        film.setGenres(genreDbStorage.readAllGenre(filmId));
+
+        return film;
+    }
+}
