fb5b242c279325b3e0419c869dd2bc642d6507d6
egolubyh <egolubyh@yandex.ru>
2022-11-12 19:23:55 +0500

    fix: new ver

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,21 +1,18 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Film {
-    private int id;
+    private long id;
     private String name;
     private String description;
     private LocalDate releaseDate;
-    private Integer duration;
-    private Set<Integer> likes = new HashSet<>();
+    private int duration;
+    private int rate;
+    private Mpa mpa;
+    private List<Genre> genres;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,21 +1,14 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
-    private int id;
-    private String email;
-    private String login;
+    private long id;
     private String name;
+    private String login;
+    private String email;
     private LocalDate birthday;
-    private Set<Integer> friends = new HashSet<>();
 }
